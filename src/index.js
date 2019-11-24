cef3713b809765b6eb0bf6f728862f73e155d04f
richard wilson <rcwilson88@gmail.com>
2019-11-23 23:04:44 -0500

    started introduction and further styling

--- src/index.js
+++ src/index.js
@@ -3,18 +3,58 @@ let doorText = document.querySelector(".door-text")
 let background = document.querySelector(".background")
 let introduction = document.querySelector(".introduction")
 let navbar = document.querySelector(".nav-bar")
+let backgroundImage = document.body.style.backgroundImage
 
 // ----DEV BUTTONS
-document.querySelector(".fade-in").addEventListener("click", () => {
-    fadeBackgroundImage("in")
-})
-document.querySelector(".fade-out").addEventListener("click", () => {
-    fadeBackgroundImage("out")
-})
-document.querySelector(".to-top").addEventListener("click", () => {
-    window.scrollTo(0, 0)
-})
+
+// document.querySelector(".fade-in").addEventListener("click", () => {
+//     fadeBackgroundImage("in")
+// })
+// document.querySelector(".fade-out").addEventListener("click", () => {
+//     fadeBackgroundImage("out")
+// })
+// document.querySelector(".to-top").addEventListener("click", () => {
+//     window.scrollTo(0, 0)
+// })
+
 // ^^^^ DEV BUTTONS
+
+
+// ==========DEV OPTIONS===========
+// fadeBackgroundImage("in")
+// setBackgroundImage('./img/textures/background-morning.png')
+// navbar.classList.add("show")
+// ================================
+
+
+
+
+
+door.addEventListener("mouseover", () => {
+    fadeBackgroundImage("in")
+    doorText.setAttribute("class", "door-text-animation")
+    door.style.animation = "door-hover 8s infinite alternate none ease-in-out";
+})
+
+// DOOR CLICK EVENT
+door.addEventListener("click", () => {
+    fadeBackgroundImage("out");
+    window.scroll({
+        top: 6017,
+        behavior: 'smooth',
+    })
+
+})
+
+window.addEventListener("scroll", () => {
+    console.log(scrollY)
+    
+    if (scrollY > 2350 && scrollY < 2800) {
+        fadeBackgroundImage("in")
+        setBackgroundImage('./img/textures/background-morning.png');
+        navbar.classList.add("show")
+       }
+})
 
 
 function fadeBackgroundImage(direction) {
@@ -34,31 +74,3 @@ function setBackgroundImage(imageUrl) {
     document.body.style.backgroundImage = `url(${imageUrl})`;
     fadeBackgroundImage("in");
 }
-let backgroundImage = document.body.style.backgroundImage
-
-
-door.addEventListener("mouseover", () => {
-    fadeBackgroundImage("in")
-    doorText.setAttribute("class", "door-text-animation")
-    door.style.animation = "door-hover 8s infinite alternate none ease-in-out";
-})
-
-door.addEventListener("click", () => {
-    fadeBackgroundImage("out");
-    introduction.scrollIntoView({
-        block: 'center',
-        inline: "center",
-        behavior: 'smooth',
-    })
-
-})
-
-window.addEventListener("scroll", () => {
-    console.log(scrollY)
-    
-    if (scrollY > 2350 && scrollY < 2800) {
-        fadeBackgroundImage("in")
-        setBackgroundImage('./img/textures/background-morning.png');
-        navbar.classList.add("show")
-       }
-})
